62a9e5c6dfe78ce8c8b5ece8f01b36a33d808c66
AkshayBokhare <138112705+AkshayBokhare@users.noreply.github.com>
2024-09-10 12:36:23 +0530

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,6 +4,8 @@ class Main{
     public static void main(String[] args) {
         
         System.out.println("Hello, Akshay this is Jenkin Demo Project ");
+       System.out.println("Hello, Shubham ");
+System.out.println("Hello,Ankit");
 
 
     }
