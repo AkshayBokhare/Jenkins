19fcfcd4097370d17fd0a5053e3328cdeeed3b27
AkshayBokhare <138112705+AkshayBokhare@users.noreply.github.com>
2024-09-10 14:12:35 +0530

    Update6 main.java

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ class Main{
 
     public static void main(String[] args) {
         
-        System.out.println("Hello, Akshay this is Jenkin Demo Project ")
+        System.out.println("Hello, Akshay this is Jenkin Demo Project ");
    
 
 
